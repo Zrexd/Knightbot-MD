03f232828cd1084289755106af584eb405aaf646
Zrexd <zrexnight@gmail.com>
2025-03-17 13:45:45 +0530

    Update massage js

--- settings.js
+++ settings.js
@@ -3,11 +3,11 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '94774000698', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
-};
+};'
 
 module.exports = settings;
